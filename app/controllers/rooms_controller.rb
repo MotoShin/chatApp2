66a0a60278412d6d696b991df895366fa50be7ea
Moto Shinriki <m8o8t2o8@gmail.com>
2017-04-15 17:24:17 +0900

    -a

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,16 @@
+class RoomsController < ApplicationController
+	def index
+		@messages = Message.all
+	end
+
+	def new
+	end
+
+	def create
+		@message = Message.new
+		@message.name = params[:message][:name]
+		@message.content = params[:message][:content]
+		@message.save
+		redirect_to root_path
+	end
+end
